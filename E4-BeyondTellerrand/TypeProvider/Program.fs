916fcc3083786277ff3b4b23e87c17123ab1bc40
Volkmar Rigo <volkmar@rigo.bz>
2022-01-16 11:55:48 +0100

    F# Migration to Net 6

--- E4-BeyondTellerrand/TypeProvider/Program.fs
+++ E4-BeyondTellerrand/TypeProvider/Program.fs
@@ -1,21 +1,19 @@
-﻿open FSharp.Data
+﻿// Mehr Infos zu Typeprovidern: https://fsprojects.github.io/FSharp.Data/
+open FSharp.Data
 
 // Definiere den neuen Typ csvTypeProvider mithilfe der CSV Datei
 type csvTypeProvider = CsvProvider<"data.csv", HasHeaders=true>
 
-[<EntryPoint>]
-let main argv =
-    
-    // Typ initialisieren
-    let csv = csvTypeProvider.GetSample()
+// Typ initialisieren
+let csv = csvTypeProvider.GetSample()
 
-    // für alle Zeilen
-    let avgNote = csv.Rows 
-                  // die Spalte Note extrahieren          
-                  |> Seq.map(fun row -> row.Note)
-                  // den Durchschnitt bilden
-                  |> Seq.averageBy (fun note -> float note)
+// für alle Zeilen
+let avgNote = csv.Rows
+                // die Spalte Note extrahieren          
+                |> Seq.map(fun row -> row.Note)
+                // den Durchschnitt bilden
+                |> Seq.averageBy (fun note -> float note)
 
-    printfn "Durchschnittsnote: %f" avgNote
+printfn "Durchschnittsnote: %.2f" avgNote
 
-    0 
+exit 0 
